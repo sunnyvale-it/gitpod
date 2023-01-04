8fd10137f36fe5cb49dab455b7022a8554c6d79b
Andrea Falzetti <andrea@gitpod.io>
2023-01-04 13:10:28 +0000

    update

--- components/gitpod-cli/cmd/build.go
+++ components/gitpod-cli/cmd/build.go
@@ -1,12 +1,19 @@
-// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Copyright (c) 2023 Gitpod GmbH. All rights reserved.
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License.AGPL.txt in the project root for license information.
 
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/gitpod-io/gitpod/common-go/util"
+	"github.com/gitpod-io/gitpod/gitpod-cli/pkg/supervisor"
+	"github.com/gitpod-io/gitpod/gitpod-cli/pkg/utils"
+	"github.com/gitpod-io/gitpod/supervisor/api"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +22,24 @@ var buildCmd = &cobra.Command{
 	Short:  "Builds the workspace image (useful to debug a workspace custom image)",
 	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("TODO")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		client, err := supervisor.New(ctx)
+		if err != nil {
+			utils.LogError(ctx, err, "Could not get workspace info required to build", client)
+			return
+		}
+		defer client.Close()
+
+		wsInfo, err := client.Info.WorkspaceInfo(ctx, &api.WorkspaceInfoRequest{})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		gitpodConfig, err := util.ParseGitpodConfig(wsInfo.CheckoutLocation)
+		fmt.Println(gitpodConfig.Image)
 	},
 }
 
